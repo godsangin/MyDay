b9ff07e534d3f59f6477483a9c9945c3180d4a84
godsangin <leein3293@naver.com>
2019-01-28 11:11:29 +0900

    MainActivity was connected with EventActivity

--- app/src/main/java/com/msproject/myhome/mydays/EventActivity.java
+++ app/src/main/java/com/msproject/myhome/mydays/EventActivity.java
@@ -3,14 +3,18 @@ package com.msproject.myhome.mydays;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 
 import java.util.ArrayList;
 
@@ -20,25 +24,33 @@ public class EventActivity extends AppCompatActivity {
     EventListAdapter eventListAdapter;
     GridView gridView;
     CategoryGridAdapter categoryGridAdapter;
-    String date;
+    ConstraintLayout titleBar;
+    String time;
     String content= "";
     int quarterNo;
     MydaysDBHelper myDaysDB = new MydaysDBHelper(this,"MyDays.db",null,1);
     CategoryDBHelper categoryDB = new CategoryDBHelper(this,"CATEGORY.db",null,1);
+
+    private final int RESPONSE_SAVE_CODE = 1;
+    private final int RESPONSE_UNSAVE_CODE = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        Intent MainIntent = getIntent();
-        date = MainIntent.getStringExtra("date");
-//        quarterNo = MainIntent.getIntExtra("quarterNo",-1);
-        quarterNo=0;
+        Intent mainIntent = getIntent();
+        time = mainIntent.getStringExtra("Date");
         eventListView = findViewById(R.id.event_listview);
         gridView  = findViewById(R.id.category_gridview);
+        titleBar = findViewById(R.id.title_bar);
+        quarterNo = Integer.parseInt(mainIntent.getStringExtra("Hour"));
+        setResult(RESPONSE_UNSAVE_CODE);
+
+        setTitleContents();
 
 
         ArrayList<Event> events = new ArrayList<>();
-        ArrayList<Event> DBEvents = myDaysDB.getEvents(date,quarterNo);
+        ArrayList<Event> DBEvents = myDaysDB.getEvents(time,quarterNo);
         ArrayList<Category> categories = categoryDB.getResult();
 
         for(int i = 0; i < 6; i++){
@@ -65,10 +77,9 @@ public class EventActivity extends AppCompatActivity {
                 eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                          @Override
                          public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                             date="190121";
                              Event clickedEvent = (Event) eventListAdapter.getItem(position);
                              int eventNo = clickedEvent.eventNo;
-                             dialog(date,eventNo,category);
+                             dialog(time,eventNo,category);
 
                          }
                     }
@@ -107,5 +118,46 @@ public class EventActivity extends AppCompatActivity {
                 });
         builder.show();
     }
+
+    public void setTitleContents(){
+        ImageView backButton = titleBar.findViewById(R.id.back_bt);
+        ImageView menuButton = titleBar.findViewById(R.id.menu_bt);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭시 팝업 메뉴가 나오게 하기
+                // PopupMenu 는 API 11 레벨부터 제공한다
+                PopupMenu p = new PopupMenu(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        v); // anchor : 팝업을 띄울 기준될 위젯
+                getMenuInflater().inflate(R.menu.menu_main, p.getMenu());
+                // 이벤트 처리
+                p.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getItemId() == R.id.statistic_graph) {//StatisicActivity로 intent
+                            Intent intent = new Intent(EventActivity.this, StatisticActivity.class);
+                            startActivity(intent);
+                        } else if (item.getItemId() == R.id.setting) {
+                            Intent intent = new Intent(EventActivity.this, SettingActivity.class);
+                            startActivity(intent);
+
+                        } else if (item.getItemId() == R.id.remove_ad) {//광고제거
+
+                        }
+                        return false;
+                    }
+                });
+                p.show(); // 메뉴를 띄우기
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
 
 }
