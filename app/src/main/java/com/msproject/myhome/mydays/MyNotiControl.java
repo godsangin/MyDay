dc7ab96a10db7728c3f2ff69f76ba0ff808ab960
godsangin <leein3293@naver.com>
2019-04-22 23:21:07 +0900

    <=oreo version notification error modified(Service)

--- app/src/main/java/com/msproject/myhome/mydays/MyNotiControl.java
+++ app/src/main/java/com/msproject/myhome/mydays/MyNotiControl.java
@@ -1,9 +1,14 @@
 package com.msproject.myhome.mydays;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.widget.RemoteViews;
 
@@ -11,6 +16,7 @@ public class MyNotiControl {
     Context context;
     RemoteViews rView;
     NotificationCompat.Builder nBuilder;
+    Notification notification;
 
     public MyNotiControl (Context parent)
     {
@@ -27,11 +33,40 @@ public class MyNotiControl {
         //set the listener
         setListener(rView);
         nBuilder.setContent(rView);
+
+        notification = nBuilder.build();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public MyNotiControl(Context parent, String NOTIFICATION_CHANNEL_ID, String channelName){//Oreo버전 이상의 서비스를 위해 notification channel 추가
+        this.context= parent;
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) parent.getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        MyNotiControl c1 = new MyNotiControl(parent);
+        nBuilder = new NotificationCompat.Builder(parent, NOTIFICATION_CHANNEL_ID);
+        notification = nBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.logo3)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .setProgress(0,0,true)
+                .build();
+        rView = new RemoteViews(parent.getPackageName(), R.layout.activity_my_noti_control); //노티바를 내렸을때 보여지는 화면입니다.
+
+        //set the listener
+        setListener(rView);
+        nBuilder.setContent(rView);
+        notification = nBuilder.build();
     }
 
     public Notification getNoti()
     {
-        return nBuilder.build();
+        return notification;
     }
 
     public void setListener(RemoteViews view){
